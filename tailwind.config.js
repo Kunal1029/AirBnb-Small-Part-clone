69d489e2f6d29499b44c9605a0dc261c1aa973fc
kunal1029 <kunal.shivhare0529@gmail.com>
2024-02-07 23:50:49 +0530

    Design

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./views/layouts/*.ejs","./views/listings/*.ejs"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
